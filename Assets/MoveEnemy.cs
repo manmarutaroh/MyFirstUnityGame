e3c30d0e5ae58fcf2e412e0effa79d1da0c24f51
taro <koakisumi0425@yahoo.co.jp>
2019-03-20 12:28:46 +0900

    FirstCommit

---
+++ Assets/MoveEnemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    private CharacterController enemyController;
+    private Vector3 destination;
+    [SerializeField]
+    private float walkspeed = 2.0f;
+    private Vector3 velocity;
+    private Vector3 direction;
+    private Vector3 startPosition;
+    public GameObject SuitMan;
+    private Vector3 manPos;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyController = GetComponent<CharacterController>();
+        //startPosition = transform.position;
+        //var randDestination = Random.insideUnitCircle * 30;
+        
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyController.isGrounded)
+        {
+            velocity = Vector3.zero;
+            manPos = SuitMan.transform.position;
+
+            direction = (manPos - transform.position).normalized;
+            velocity = direction * walkspeed;
+            transform.LookAt(new Vector3(manPos.x, transform.position.y, manPos.z));
+            
+        }
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+        enemyController.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/PlayerContoroller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerContoroller : MonoBehaviour
+{
+    int count;
+    public Text countText;
+    public Text clearText;
+    public Button retryButton;
+    public Text gameOverText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 5)
+        {
+            
+            SceneManager.LoadScene("ClearSceane");
+
+        }
+
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Items")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+
+        if (other.tag == "Enemy")
+        {
+            
+            SceneManager.LoadScene("GameOverSceane");
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "ゲット数:"+count.ToString();
+    }
+
+    public void RetryStage()
+    {
+        SceneManager.LoadScene("GameSceane");
+    }
+    
+}
